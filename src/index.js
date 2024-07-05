9caba7fc3a006b81d09f9af684868df8d1ee4d6b
OlaVasylieva7 <124078842+OlaVasylieva7@users.noreply.github.com>
2024-07-05 23:53:33 +0200

    add styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import { alert } from "@pnotify/core";
 import "@pnotify/core/dist/PNotify.css";
 
+import Chart from "chart.js/auto";
+
+
 const keys = ["!", "o", "v", "7", "5", "t", "$", "#", "k", "}"];
 
 let currentKeyIndex = 0;
@@ -55,16 +58,22 @@ button.addEventListener("click", startAgain);
 
 // task 2
 
-// const chartData = {
-//   labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
-//   datasets: [
-//     {
-//       label: "Продажі за останній місяць",
-//       data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
-//       backgroundColor: "#2196f3",
-//       borderColor: "#2196f3",
-//       borderWidth: 1,
-//     },
-//   ],
-// };
-// const salesChart = new Chart();
+const chart = document.getElementById('sales-chart');
+
+const chartData = {
+  labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+  datasets: [
+    {
+      label: "Продажі за останній місяць",
+      data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
+      backgroundColor: "#2196f3",
+      borderColor: "#2196f3",
+      borderWidth: 1,
+    },
+  ],
+};
+
+const salesChart = new Chart(chart, {
+  type: 'line',
+  data: chartData
+});
